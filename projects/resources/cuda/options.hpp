a3a3f412345f729fc78e8f69924fe139d1d20d67
QiZhou1512 <qi1.zhou@mail.polimi.it>
2020-09-30 17:49:22 +0200

    experimenting with multi thread and multi gpu

--- projects/resources/cuda/options.hpp
+++ projects/resources/cuda/options.hpp
@@ -40,6 +40,7 @@ enum BenchmarkEnum {
     B7,
     B8,
     B10,
+    B11,
     ERR
 };
 
@@ -72,6 +73,8 @@ inline BenchmarkEnum get_benchmark(std::string benchmark) {
         return BenchmarkEnum::B8;
     else if (benchmark == "b10")
         return BenchmarkEnum::B10;
+    else if (benchmark == "b11")
+        return BenchmarkEnum::B11;
     else
         return BenchmarkEnum::ERR;
 }
@@ -97,9 +100,10 @@ struct Options {
     //////////////////////////////
     //////////////////////////////
 
-    Options(int argc, char *argv[]) {
-        map_init(policy_map)(Policy::Sync, "sync")(Policy::Async, "default")(Policy::CudaGraph, "cudagraph")(Policy::CudaGraphAsync, "cudagraphmanual")(Policy::CudaGraphSingle, "cudagraphsingle");
-        map_init(benchmark_map)(BenchmarkEnum::B1, "b1")(BenchmarkEnum::B5, "b5")(BenchmarkEnum::B6, "b6")(BenchmarkEnum::B7, "b7")(BenchmarkEnum::B8, "b8")(BenchmarkEnum::B10, "b10");
+    Options(int argc, char *argv[])
+    {
+        map_init(policy_map)(Policy::Sync, "sync")(Policy::Async, "default")(Policy::CudaGraph, "cudagraph")(Policy::CudaGraphAsync, "cudagraph_manual");
+        map_init(benchmark_map)(BenchmarkEnum::B1, "b1")(BenchmarkEnum::B5, "b5")(BenchmarkEnum::B6, "b6")(BenchmarkEnum::B7, "b7")(BenchmarkEnum::B8, "b8")(BenchmarkEnum::B10, "b10")(BenchmarkEnum::B11, "b11");
 
         int opt;
         static struct option long_options[] = {{"debug", no_argument, 0, 'd'},
