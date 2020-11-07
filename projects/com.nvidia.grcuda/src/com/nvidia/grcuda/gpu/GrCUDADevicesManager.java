d9604df3ab7f6c18bcb5aa75786da4c387e96f4e
QiZhou1512 <qi1.zhou@mail.polimi.it>
2020-11-07 21:33:15 +0100

    working on gpus manager

--- projects/com.nvidia.grcuda/src/com/nvidia/grcuda/gpu/GrCUDADevicesManager.java
+++ projects/com.nvidia.grcuda/src/com/nvidia/grcuda/gpu/GrCUDADevicesManager.java
@@ -9,20 +9,33 @@ public class GrCUDADevicesManager {
     private final CUDARuntime runtime;
     private final ArrayList<Device> deviceArrayList = new ArrayList<>();
     private final Integer numberOfGPUs;
+    private Integer currentDeviceId;
     public GrCUDADevicesManager(CUDARuntime runtime){
         this.runtime = runtime;
         this.numberOfGPUs = runtime.cudaGetDeviceCount();
-
-        for(int i = 0; i<numberOfGPUs;i++) {
-            deviceArrayList.add(new Device(i, runtime));
-        }
-
-
+        this.currentDeviceId = runtime.cudaGetDevice();
+        initDevices();
     }
 
 
 
     private void initDevices(){
-        runtime.cudaGetDeviceCount();
+        for(int i = 0; i<numberOfGPUs;i++) {
+            deviceArrayList.add(new Device(i, runtime));
+        }
     }
+
+    public int getCurrentDeviceId(){
+        return this.currentDeviceId;
+    }
+
+    public int getNumberOfGPUs(){
+        return this.numberOfGPUs;
+    }
+
+    public void setDevice(int id){
+        runtime.cudaSetDevice(id);
+        this.currentDeviceId = id;
+    }
+
 }
